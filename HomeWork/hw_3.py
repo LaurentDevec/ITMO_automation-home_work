2d0ec18ef90dfd909cd643b0af4c42c1b5e068f5
LaurentDEveck <vasilevartem91@yandex.ru>
2025-10-08 23:43:19 +0300

    Third homework done!

--- HomeWork/hw_3.py
+++ HomeWork/hw_3.py
@@ -4,51 +4,47 @@ def find_max_number(num1, num2):
     else:
         print(num2)
 
-        # 3. Функция на вход получает два произвольных числа. Вывести в консоль "yes", если они отличаются друг от друга на 135, иначе вывести на экран "No"
-        def check_difference(num1, num2):
-            if abs(num1 - num2) == 135:
-                print("yes")
-            else:
-                print("No")
+# 3. Функция на вход получает два произвольных числа. Вывести в консоль "yes", если они отличаются друг от друга на 135, иначе вывести на экран "No"
+def check_difference(num1, num2):
+    if abs(num1 - num2) == 135:
+        print("yes")
+    else:
+        print("No")
 
-        # 4. Функция на вход получает произвольное число от 1 до 12 (номер месяца). Вывести название сезона года в консоль (зима, весна, лето, осень)
-        def get_season(month):
-            if month in [12, 1, 2]:
-                print("зима")
-            elif month in [3, 4, 5]:
-                print("весна")
-            elif month in [6, 7, 8]:
-                print("лето")
-            elif month in [9, 10, 11]:
-                print("осень")
-            else:
-                print("Неверный номер месяца")
+# 4. Функция на вход получает произвольное число от 1 до 12 (номер месяца). Вывести название сезона года в консоль (зима, весна, лето, осень)
+def get_season(month):
+    if month in [12, 1, 2]:
+        print("зима")
+    elif month in [3, 4, 5]:
+        print("весна")
+    elif month in [6, 7, 8]:
+        print("лето")
+    elif month in [9, 10, 11]:
+        print("осень")
+    else:
+        print("Неверный номер месяца")
 
-        # 5. Функция на вход получает три произвольных числа. Если все числа больше 10, то вывести на экран "yes", иначе "no"
-        def check_all_greater_than_ten(num1, num2, num3):
-            if num1 > 10 and num2 > 10 and num3 > 10:
-                print("yes")
-            else:
-                print("no")
+# 5. Функция на вход получает три произвольных числа. Если все числа больше 10, то вывести на экран "yes", иначе "no"
+def check_all_greater_than_ten(num1, num2, num3):
+    if num1 > 10 and num2 > 10 and num3 > 10:
+        print("yes")
+    else:
+        print("no")
 
-        # 6. Функция на вход получает список, состоящий из 5 произвольных чисел. Найти количество положительных чисел среди них.
-        def count_positive_numbers(numbers):
-            count = 0
-            for num in numbers:
-                if num > 0:
-                    count += 1
-            print(count)
+# 6. Функция на вход получает список, состоящий из 5 произвольных чисел. Найти количество положительных чисел среди них.
+def count_positive_numbers(numbers):
+    count = 0
+    for num in numbers:
+        if num > 0:
+            count += 1
+    print(count)
 
-            # Или более компактный вариант:
-            # positive_count = sum(1 for num in numbers if num > 0)
-            # print(positive_count)
-
-        # 7. Функция на вход получает 2 переменные: кол-во лет (int) и кол-во месяцев (int)
-        # Вывести в консоль количество дней за это время. Считать, что в каждом месяце 29 дней.
-        def calculate_days(years, months):
-            total_months = years * 12 + months
-            total_days = total_months * 29
-            print(total_days)
+# 7. Функция на вход получает 2 переменные: кол-во лет (int) и кол-во месяцев (int)
+# Вывести в консоль количество дней за это время. Считать, что в каждом месяце 29 дней.
+def calculate_days(years, months):
+    total_months = years * 12 + months
+    total_days = total_months * 29
+    print(total_days)
 
         # Примеры использования функций:
         print("Задача 2:")
