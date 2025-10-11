56447948fd7b8573605dfb730ec8a145a0cc0ec7
LaurentDEveck <vasilevartem91@yandex.ru>
2025-10-11 11:45:36 +0300

    Fifth HW !

--- HomeWork/hw_5.py
+++ HomeWork/hw_5.py
@@ -8,17 +8,15 @@ def test_saucedemo_login():
     # a. Переходим на страницу
     browser.open('https://www.saucedemo.com/')
 
-    # b. Находим элементы
     username_field = browser.element('#user-name')
     password_field = browser.element('#password')
     submit_button = browser.element('#login-button')
 
-    # c. Проверяем, что элементы найдены
     if username_field.matching(be.visible) and password_field.matching(be.visible) and submit_button.matching(
             be.visible):
         print("Элементы найдены")
 
-        # Дополнительно: проверяем плейсхолдеры
+        # проверяем плейсхолдеры
         username_field.should(have.attribute('placeholder').value('Username'))
         password_field.should(have.attribute('placeholder').value('Password'))
         submit_button.should(have.attribute('value').value('Login'))
@@ -27,10 +25,9 @@ def test_saucedemo_login():
     else:
         print("Не все элементы найдены")
 
-    # Закрываем браузер
     browser.quit()
 
 
-# Запуск функции
+
 if __name__ == "__main__":
     test_saucedemo_login()
