d1c49b0419e08be0e29a1886f747e5c1626ffc67
LaurentDevec (Artemy Vasilev) <61679427+LaurentDevec@users.noreply.github.com>
2025-10-22 22:19:07 +0300

    HW_8
    HW_8

--- PythonProject1/PythonProject/demoqa_home/pages/accordion.py
+++ PythonProject1/PythonProject/demoqa_home/pages/accordion.py
@@ -2,15 +2,15 @@ from selenium.webdriver.common.by import By
 from pages.base_page import BasePage  # предполагаем, что есть базовый класс
 
 class Accordion(BasePage):
-    URL = "https://demoqa.com/accordian"
+    url = "https://demoqa.com/accordian"
 
     # Локаторы элементов
-    SECTION1_HEADING = (By.ID, "section1Heading")
-    SECTION1_CONTENT = (By.CSS_SELECTOR, "#section1Content > p")
+    section1_heading = (By.ID, "section1Heading")
+    section1_content = (By.CSS_SELECTOR, "#section1Content > p")
 
-    SECTION2_CONTENT_1 = (By.CSS_SELECTOR, "#section2Content > p:nth-child(1)")
-    SECTION2_CONTENT_2 = (By.CSS_SELECTOR, "#section2Content > p:nth-child(2)")
-    SECTION3_CONTENT = (By.CSS_SELECTOR, "#section3Content > p")
+    section2_content_1 = (By.CSS_SELECTOR, "#section2Content > p:nth-child(1)")
+    section2_content_2 = (By.CSS_SELECTOR, "#section2Content > p:nth-child(2)")
+    section3_content = (By.CSS_SELECTOR, "#section3Content > p")
 
     # Методы для взаимодействия
     def open(self):
@@ -29,4 +29,5 @@ class Accordion(BasePage):
         return self.browser.find_element(*self.SECTION2_CONTENT_2).is_displayed()
 
     def is_section3_content_visible(self):
+
         return self.browser.find_element(*self.SECTION3_CONTENT).is_displayed()
